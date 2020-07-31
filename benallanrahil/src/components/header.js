fc1145780c21c6b8d64dd6861dc491507c69bc7d
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-07-30 22:03:01 -0400

    added raleway fonts

--- benallanrahil/src/components/header.js
+++ benallanrahil/src/components/header.js
@@ -9,6 +9,10 @@ const Header = ({ siteTitle }) => (
       marginBottom: `1.45rem`,
     }}
   >
+    <link
+      href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,800;1,100;1,200;1,300;1,400;1,500&display=swap"
+      rel="stylesheet"
+    ></link>
     <div
       style={{
         margin: `0 auto`,
