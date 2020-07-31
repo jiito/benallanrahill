cf4f8f9f7af583316016514c4f4a0206f34a70ed
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-07-30 22:11:50 -0400

    initial commit

---
+++ benallanrahil/src/components/landing_page/Hero.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Hero = () => {
+  return (
+    <div className="Hero">
+      <img src="../../../public/static/large.png" alt="me" />
+    </div>
+  )
+}
+
+export default Hero
