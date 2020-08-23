669bddd20b417b2f0e3ceda5d70695b810fcbe74
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-08-22 23:22:56 -0400

    added packages

--- benallanrahil/gatsby-config.js
+++ benallanrahil/gatsby-config.js
@@ -27,6 +27,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-styled-components`,
+    //   resolve: `gatsby-plugin-typography`, // {
+    //   options: {
+    //     pathToConfigModule: `src/utils/typography`,
+    //   },
+    // },
     `gatsby-plugin-sass`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
