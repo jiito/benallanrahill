1d37adf53dc379a675e069c32bdd068d437a5459
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-09-23 23:09:22 -0400

    inital scaffolding

--- benallanrahil-client/src/components/landing_page/LogoPortfolio.js
+++ benallanrahil-client/src/components/landing_page/LogoPortfolio.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import styled from "styled-components"
+
+const Grid = styled.div`
+    display: flex;
+    flex-direction: col
+    align-items: center;
+    background-color: orange;
+`
+
+const Logo = styled.img`
+
+`
 
 const LogoPortfolio = () => {
     return (
-        <div>
-            
-        </div>
-    );
+      <div>
+        <Grid>
+          <img src="https://via.placeholder.com/150" />
+          <img src="https://via.placeholder.com/150" />
+          <img src="https://via.placeholder.com/150" />
+          <img src="https://via.placeholder.com/150" />
+        </Grid>
+      </div>
+    )
 }
 
 export default LogoPortfolio;
