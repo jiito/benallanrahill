ea7642791a31de193cc664bf14bccd0eda2be6b7
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-09-25 20:00:05 -0400

    inital commit

---
+++ benallanrahil-client/src/components/landing_page/Portfolio.js
@@ -0,0 +1,20 @@
+import React from "react"
+import styled from "styled-components"
+
+const Portfolio = () => {
+  return (
+    <PortfolioDiv>
+      <h1>What I am working on</h1>
+    </PortfolioDiv>
+  )
+}
+
+const PortfolioDiv = styled.div`
+  display: flex;
+  font-family: sans-serif;
+  font-weight: lighter;
+  padding: 2em;
+  margin-top: 20px;
+`
+
+export default Portfolio
