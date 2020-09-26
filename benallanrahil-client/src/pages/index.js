51dfc2d69e369876964b7387563af93f52b9342d
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-09-25 20:00:34 -0400

    moved portfolio comp

--- benallanrahil-client/src/pages/index.js
+++ benallanrahil-client/src/pages/index.js
@@ -7,14 +7,8 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 import Hero from "../components/landing_page/Hero"
 import LogoPortfolio from "../components/landing_page/LogoPortfolio"
+import Portfolio from "../components/landing_page/Portfolio.js"
 
-const Portfolio = styled.div`
-  display: flex;
-  font-family: sans-serif;
-  font-weight: lighter;
-  padding: 2em;
-  background-color: aliceblue;
-`
 
 const IndexPage = () => (
   <Layout>
